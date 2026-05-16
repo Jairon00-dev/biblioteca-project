ccf52336a6ad330ef7458291ffba78bb83eb582c
Jairon00-dev <jaironalves085@gmail.com>
2026-05-15 23:38:13 -0300

    Adjusts in code.

--- src/main/java/com/projeto/biblioteca/backend/domain/model/Categoria.java
+++ src/main/java/com/projeto/biblioteca/backend/domain/model/Categoria.java
@@ -18,7 +18,6 @@ public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     private String nome;
